4c1d1fd9053f3d1c5030dea67d77315b13d7dacc
unknown <ppekar2001@gmail.com>
2022-04-08 09:41:15 +0200

    masodik valtoytatas

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -82,6 +82,7 @@ public class GT4500 implements SpaceShip {
         firingSuccess =(secondaryTorpedoStore.fire(1) ||  primaryTorpedoStore.fire(1))   ;
         //wasPrimaryFiredLast = true;
         break;
+        //masodik valtoztatas
     }
 
     return firingSuccess;
